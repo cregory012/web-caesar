5a8cdd9716253572f82ec9b4ede1b01c6d72c6b0
Craig McElwain <cregory012@yahoo.com>
2019-06-18 20:33:04 -0500

    added form redirect

--- main.py
+++ main.py
@@ -11,34 +11,34 @@ form = """
     <head>
         <style>
 
-            input {
+            input {{
                 display:inline-block;
-            }
+            }}
 
-            label {
+            label {{
                 display:inline-block;
-            }
+            }}
 
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
     <body>
         <form action="/" method = "POST">
             <label for="rot" ><h3>Rotate by:</h3></label>
             <input type="text" name="rot" value="0" id="rot"/>
-            <textarea name="text" rows="5" col="30"></textarea>
+            <textarea name="text" rows="5" col="30">{0}</textarea>
             <input type="submit"/>
         </form>
     </body>
@@ -47,14 +47,14 @@ form = """
 
 @app.route("/")
 def index():
-    return form
+    return form.format("")
 
 @app.route("/" , methods=["POST"])
 def encrypt():
     text = request.form['text']
     rot = int(request.form["rot"])
     rotated_string = rotate_string(text, rot)
-    return ("<h1>" + rotated_string + "</h1>")
+    return form.format(rotated_string)
 
 
 app.run()    
